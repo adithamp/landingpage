8e7a60f09bf53af2d66d83ec86d08aa4810dcddc
adithamp <adithamp30@gmail.com>
2024-05-15 19:54:15 +0700

    menampilkan data paket harga sesuai data base

--- app/Views/solusi.php
+++ app/Views/solusi.php
@@ -6,29 +6,29 @@ Solusi
 
 <div class="col-12 d-flex justify-content-center">
     <div class="col-10 shadow" style="margin-top: 50px; height: auto; ">
-            
-            <div class="col-12 p-5 shadow">
+
+        <div class="col-12 p-5 shadow">
             <div class="col-12 text-center">
                 <p class="mb-lg-3" style="font-size: 40px; font-weight: 500;">Solusi</p>
                 <p>Beberapa contoh solusi untuk memudahkan dalam menjalan kan bisnis</p>
             </div>
-                <div class="col-12 md-4 d-flex gap-2 flex-wrap justify-content-center">
-                    <?php foreach ($solusi as $key => $value) { ?>
-                        <div class="card  p-2" style="background-color: white ; border-radius: 10px; width: 297.5px;">
-                            <div class="col-12 d-flex justify-content-center">
-                                <img src="<?= $value['gambar'] ?>" alt="" width="120px" height="120px">
-                            </div>
-                            <div class="card-body p-3 text-center">
-                                <h4 class="card-title"><b><?= $value['nama_solusi'] ?></b></h4>
-                                <p class="card-text"><?= $value['deskripsi'] ?></p>
-                                <form action="/fitur">
-                                    <button class="btn btn-lg btn btn-block">Fitur</button>
-                                </form>
-                            </div>
+            <div class="col-12 md-4 d-flex gap-2 flex-wrap justify-content-center">
+                <?php foreach ($solusi as $key => $value) { ?>
+                    <div class="card  p-2" style="background-color: white ; border-radius: 10px; width: 297.5px;">
+                        <div class="col-12 d-flex justify-content-center">
+                            <img src="<?= $value['gambar'] ?>" alt="" width="120px" height="120px">
                         </div>
-                    <?php } ?>
-                </div>
+                        <div class="card-body p-3 text-center">
+                            <h4 class="card-title"><b><?= $value['nama_solusi'] ?></b></h4>
+                            <p class="card-text"><?= $value['deskripsi'] ?></p>
+                            <form action="/fitur">
+                                <button class="btn btn-lg btn btn-block">Fitur</button>
+                            </form>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
+        </div>
     </div>
 </div>
 
@@ -38,55 +38,26 @@ Solusi
             <p style="font-size: 40px; font-weight: 500;">Paket Harga</p>
         </div>
         <div class="row justify-content-center">
-            <div class="col-md-4 mb-3">
-                <div class="card" style="background-color: white; border-radius: 10px;">
-                    <div class="card-body">
-                        <h4 class="card-title text-center"><b>Paket Harga Dokter</b></h4>
-                        <p class="card-text text-center" style="font-weight: 600; font-size: 20px;">Standar</p>
-                        <hr>
-                        <p class="card-text text-center" style="font-weight: 600;font-size: 20px;">Rp. 1,5 Juta</p>
-                        <p class="card-text text-center" style="font-weight: 600;font-size: 20px;">Mendapatkan Modul</p>
-                        <ul class="list-unstyled">
-                            <li class="mb-2"><i class="ti ti-check text-success"></i> Admisi</li>
-                            <li class="mb-2"><i class="ti ti-check text-success"></i> Dashboard</li>
-                            <li><i class="ti ti-check text-success"></i> Booking Online</li>
-                        </ul>
+            <?php foreach ($paketharga as $key => $value) { ?>
+                <div class="col-md-4 mb-3">
+                    <div class="card" style="background-color: white; border-radius: 10px;">
+                        <div class="card-body">
+                            <h4 class="card-title text-center"><b>Paket Harga <?= $value['nama_paket'] ?></b></h4>
+                            <p class="card-text text-center" style="font-weight: 600; font-size: 20px;"><?= $value['kategori_harga'] ?></p>
+                            <hr>
+                            <p class="card-text text-center" style="font-weight: 600;font-size: 20px;">Rp. <?= $value['harga']?> Juta</p>
+                            <p class="card-text text-center" style="font-weight: 600;font-size: 20px;">Mendapatkan Modul</p>
+                            <ul class="list-unstyled">
+                                <li class="mb-2"><i class="ti ti-check text-success"></i> Admisi</li>
+                                <li class="mb-2"><i class="ti ti-check text-success"></i> Dashboard</li>
+                                <li><i class="ti ti-check text-success"></i> Booking Online</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 mb-3">
-                <div class="card" style="background-color: white; border-radius: 10px;">
-                    <div class="card-body">
-                        <h4 class="card-title text-center"><b>Paket Harga Klinik</b></h4>
-                        <p class="card-text text-center" style="font-weight: 600; font-size: 20px;">Standar</p>
-                        <hr>
-                        <p class="card-text text-center" style="font-weight: 600;font-size: 20px;">Rp. 1,5 Juta</p>
-                        <p class="card-text text-center" style="font-weight: 600;font-size: 20px;">Mendapatkan Modul</p>
-                        <ul class="list-unstyled">
-                            <li class="mb-2"><i class="ti ti-check text-success"></i> Admisi</li>
-                            <li class="mb-2"><i class="ti ti-check text-success"></i> Dashboard</li>
-                            <li><i class="ti ti-check text-success"></i> Booking Online</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-3">
-                <div class="card" style="background-color: white; border-radius: 10px;">
-                    <div class="card-body">
-                        <h4 class="card-title text-center"><b>Paket Harga RumahSakit</b></h4>
-                        <p class="card-text text-center" style="font-weight: 600; font-size: 20px;">Standar</p>
-                        <hr>
-                        <p class="card-text text-center" style="font-weight: 600;font-size: 20px;">Rp. 1,5 Juta</p>
-                        <p class="card-text text-center" style="font-weight: 600;font-size: 20px;">Mendapatkan Modul</p>
-                        <ul class="list-unstyled">
-                            <li class="mb-2"><i class="ti ti-check text-success"></i> Admisi</li>
-                            <li class="mb-2"><i class="ti ti-check text-success"></i> Dashboard</li>
-                            <li><i class="ti ti-check text-success"></i> Booking Online</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <?php } ?>
         </div>
+
     </div>
 </div>
 
