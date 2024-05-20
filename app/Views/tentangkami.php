f3c264cac2dec2f2e9cd0ee3820a81a5855848e2
adithamp <adithamp30@gmail.com>
2024-05-20 16:38:48 +0700

    merapihkan tampilan

--- app/Views/tentangkami.php
+++ app/Views/tentangkami.php
@@ -4,25 +4,28 @@ Solusi
 <?php $this->endsection() ?>
 <?php $this->section('content') ?>
 
-<div class="d-flex">
-    <div class="" style="margin-top: 180px;margin-left: 50px;">
-        <h2>PT Goldstep Teknologi Indonesia</h2>
-        <p>default</p>
+<div class="container mt-4">
+    <div class="d-flex justify-content-end align-items-center">
+        <div style="margin-top: 50px; margin-right: 50px;">
+            <h2>PT Goldstep Teknologi Indonesia</h2>
+            <p>default</p>
+        </div>
+        <img src="image/cici.jpg" class="img-fluid" style="max-width: 50%; height: auto; margin-left: auto;">
     </div>
-    <img src="image/cici.jpg" style=" max-width: 50%;height: auto;margin-left: 155px;">
-</div>
-<div class="container mt-5">
-    <div class="col-12 d-flex flex-wrap gap-2 justify-content-center">
+    <div class="row mt-5">
         <?php foreach ($tentangkami as $key => $value) { ?>
-            <div class="card shadow col" style="padding: 24px; height: auto; width: 100%; flex: 1 0 500px;">
-                <div class="textarea1">
-                    <p><?= $value['judul'] ?></p>
-                </div>
-                <div class="card-body">
-                    <p><?= $value['deskripsi'] ?></p>
+            <div class="col-12 col-md-6 col-lg-6 mb-4" style="padding: 20xx; height: auto; width: 100%; flex: 1 0 500px;">
+                <div class="card shadow" style="height: 100%;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $value['judul'] ?></h5>
+                        <p class="card-text"><?= $value['deskripsi'] ?></p>
+                    </div>
                 </div>
             </div>
         <?php } ?>
     </div>
 </div>
-    <?php $this->endsection() ?>
+
+
+
+<?php $this->endsection() ?>
