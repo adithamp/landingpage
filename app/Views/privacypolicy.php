59b7941bf4d97333a0982dd47f2c13cdc1c00fa6
adithamp <adithamp30@gmail.com>
2024-05-29 15:57:11 +0700

    menghubungkan ke dalam database

--- app/Views/privacypolicy.php
+++ app/Views/privacypolicy.php
@@ -6,28 +6,9 @@ Privacy Policy
 
 
 <div class="container card text-center p-4" style="margin-top: 30px ;">
-    <div>
-    <h2>Privacy Policy</h2>
-    <br></br>
-    </div>
-<section>
-    <h2>Informasi yang Kami Kumpulkan</h2>
-    <p>Kami mengumpulkan informasi dari Anda saat Anda mendaftar di situs kami atau mengisi formulir.</p>
-    <p>Saat memesan atau mendaftar di situs kami, jika sesuai, Anda mungkin diminta untuk memasukkan: nama, alamat email, alamat surat-menyurat, atau nomor telepon.</p>
-</section>
-<section>
-    <h2>Bagaimana Kami Menggunakan Informasi Anda</h2>
-    <p>Informasi yang kami kumpulkan dari Anda dapat digunakan dalam salah satu cara berikut:</p>
-    <ul>
-        <p>Untuk mempersonalisasi pengalaman Anda (informasi Anda membantu kami merespons kebutuhan individu Anda dengan lebih baik)</p>
-        <p>Untuk meningkatkan situs web kami (kami terus berupaya meningkatkan penawaran situs web kami berdasarkan informasi dan umpan balik yang kami terima dari Anda)</p>
-        <p>Untuk meningkatkan layanan pelanggan (informasi Anda membantu kami merespons permintaan layanan pelanggan dan kebutuhan dukungan Anda dengan lebih efektif)</p>
-    </ul>
-</section>
-<section>
-    <h2>Bagaimana Kami Melindungi Informasi Anda</h2>
-    <p>Kami menerapkan berbagai langkah keamanan untuk menjaga keamanan informasi pribadi Anda saat Anda memasukkan, mengirimkan, atau mengakses informasi pribadi Anda.</p>
-</section>
-
-</div>
-<?php $this->endsection() ?>
+    <?php foreach ($privacypolicy as $key => $value) { ?>
+        <div>
+            <p><?= $value['deskripsi'] ?></p>
+        </div>
+    <?php } ?>
+    <?php $this->endsection() ?>
